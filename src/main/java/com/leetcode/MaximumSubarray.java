e4a514d7c74c6d0dc0361a444ea73a96c572dcd4
wangjf <wangjf@koal.com>
2017-07-10 14:57:22 +0800

    提交代码。

---
+++ src/main/java/com/leetcode/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+/**
+ * 53. Maximum Subarray
+ * @link https://leetcode.com/problems/maximum-subarray/#/description
+ *
+ * @author wangjf
+ * @date 2017/7/10.
+ */
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new MaximumSubarray().maxSubArray(nums));
+    }
+
+    public int maxSubArray(int[] nums) {
+        //TODO 查找子数列中的最大值，数据结构一书中有涉及
+        return 0;
+    }
+
+}
