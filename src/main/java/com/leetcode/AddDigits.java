9d536b53cd05ca3703429e6895b362d4ec079e8f
Bill <remixknighx@sina.cn>
2017-07-25 22:27:19 +0800

    提交代码。

---
+++ src/main/java/com/leetcode/AddDigits.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+/**
+ * 258. Add Digits
+ * @link https://leetcode.com/problems/add-digits/#/description
+ *
+ * @author wangjf
+ * @date 2017 - 07 - 25 22:15
+ */
+public class AddDigits {
+
+    public static void main(String[] args) {
+        AddDigits addDigits = new AddDigits();
+        System.out.println(addDigits.addDigits(58));
+    }
+
+    public int addDigits(int num) {
+        while (num / 10 != 0) {
+            int temp = num;
+            num = 0;
+            while (temp / 10 != 0) {
+                num += temp % 10;
+                temp = temp / 10;
+            }
+            num += temp;
+        }
+
+        return num;
+    }
+
+}
---
+++ src/test/com/leetcode/AddDigitsTest.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wangj on 2017/7/25 0025.
+ */
+public class AddDigitsTest {
+
+    AddDigits addDigits;
+
+    @Before
+    public void setUp() throws Exception {
+        addDigits = new AddDigits();
+    }
+
+    @Test
+    public void testAddDigits() throws Exception {
+        assertEquals(4, addDigits.addDigits(58));
+        assertEquals(8, addDigits.addDigits(17));
+        assertEquals(0, addDigits.addDigits(0));
+        assertEquals(3, addDigits.addDigits(453));
+    }
+
+}
