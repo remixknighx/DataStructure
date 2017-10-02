23d5ba1c1145c431559b43cfd57a824733136bbb
wangjf <remixknighx@sina.cn>
2017-10-02 22:58:13 +0800

    提交代码。

---
+++ src/main/java/com/leetcode/NumOfSegments.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+/**
+ * 434. Number Of Segments in a String
+ * @link https://leetcode.com/problems/number-of-segments-in-a-string/description/
+ *
+ * @author wangjf
+ * @date 2017/10/2 0002.
+ */
+public class NumOfSegments {
+
+    public static void main(String[] args) {
+        NumOfSegments numOfSegments = new NumOfSegments();
+        String s = "Hello, my name is John";
+        System.out.println(numOfSegments.countSegments(s));
+    }
+
+    public int countSegments(String s) {
+        int cnt=0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(i == 0 && s.charAt(i) != ' ') cnt++;
+            //如果当前不是空格,而前一个是空格,+1
+            if(i>0 && s.charAt(i-1) == ' ' && s.charAt(i) != ' ')
+                cnt++;
+        }
+
+        return cnt;
+    }
+
+}
---
+++ src/test/com/leetcode/ThirdMaxNumberTest.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wangjf
+ * @date 2017/10/2 0002.
+ */
+public class ThirdMaxNumberTest {
+
+    ThirdMaxNumber thirdMaxNumber;
+
+    @Before
+    public void setUp() throws Exception {
+        thirdMaxNumber = new ThirdMaxNumber();
+    }
+
+    @Test
+    public void testThirdMax() throws Exception {
+        int[] nums1 = {4, 3, 5, 2, 5};
+        assertEquals(3, thirdMaxNumber.thirdMax(nums1));
+
+        int[] nums2 = {3};
+        assertEquals(3, thirdMaxNumber.thirdMax(nums2));
+
+        int[] nums3 = {1, 2};
+        assertEquals(2, thirdMaxNumber.thirdMax(nums3));
+
+        int[] nums4 = {1, 2, 3, 4, 5};
+        assertEquals(3, thirdMaxNumber.thirdMax(nums4));
+
+        int[] nums5 = {1, 2, -2147483648};
+        assertEquals(-2147483648, thirdMaxNumber.thirdMax(nums5));
+    }
+
+}
