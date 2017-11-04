22b999102e0d7e3c5a87786d04a8b136226a8649
wangjf <remixknighx@sina.cn>
2017-11-04 19:52:20 +0800

    commit code

--- src/main/java/com/leetcode/StringCompression.js
+++ src/main/java/com/leetcode/StringCompression.js
@@ -1,9 +1,36 @@
 /**
+ * 443. String Compression
+ * @link https://leetcode.com/problems/string-compression/description/
+ *
  * @param {character[]} chars
  * @return {number}
  */
 var compress = function(chars) {
+    let newChars = "";
+    let oldChar = chars[0];
+    newChars += oldChar;
 
+    let count = 1;
+
+    for (let i = 1; i < chars.length; i++) {
+        if(chars[i] == oldChar){
+            count++;
+        } else {
+            if(count !== 1) {
+                newChars+= count;
+            }
+
+            oldChar = chars[i];
+            newChars += oldChar;
+            count = 1;
+        }
+    }
+
+    if(count !== 1) {
+        newChars += count;
+    }
+
+    return newChars.split("");
 };
 
 var chars = ["a","a","b","b","c","c","c"];
