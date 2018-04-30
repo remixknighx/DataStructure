1edc0e8e76bf7e27c06d9979e2ff2c4535b3ea78
wangjianfeng <remixknighx@sina.cn>
2018-04-30 20:59:56 +0800

    commit code

---
+++ src/main/java/com/leetcode/ConstructRectangle.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+/**
+ * 492. Construct the Rectangle
+ * @link https://leetcode.com/problems/construct-the-rectangle/description/
+ *
+ * @author wangjf
+ * @date 2018/4/30 0030.
+ */
+public class ConstructRectangle {
+
+    public static void main(String[] args) {
+        int area = 3;
+        int[] result = new ConstructRectangle().constructRectangle(area);
+        System.out.println("length = " + result[0] + " width = " + result[1]);
+    }
+
+    public int[] constructRectangle(int area) {
+        int[] result = new int[2];
+
+        int smallDiff = Integer.MAX_VALUE;
+        for (int l = 1; l <= area; l++) {
+            int w = area / l;
+            if(area % l == 0){
+                if(l - w >= 0 && l - w < smallDiff){
+                    smallDiff = l - w;
+                    result[0] = l;
+                    result[1] = w;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
