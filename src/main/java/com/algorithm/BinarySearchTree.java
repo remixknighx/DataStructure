7acf44f67752dd6d125d7010afeece8717a7c756
wangjianfeng <remixknighx@sina.cn>
2020-04-13 23:20:14 +0800

    二叉搜索树

---
+++ src/main/java/com/algorithm/BinarySearchTree.java
@@ -0,0 +1,116 @@
+package com.algorithm;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+
+        TreeNode root2 = new TreeNode(4);
+        root2.left = new TreeNode(5);
+        root2.right = new TreeNode(6);
+
+        System.out.println(isSameTree(root, root2));
+        System.out.println(isValidBST(root));
+    }
+
+    /**
+     * 给所有值加1
+     */
+    public static void plusOne(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        root.val += 1;
+
+        plusOne(root.left);
+        plusOne(root.right);
+    }
+
+    /**
+     * 判断两棵树是否完全相同
+     */
+    private static boolean isSameTree(TreeNode root1, TreeNode root2) {
+        // 都为空的话，显然相同
+        if (root1 == null && root2 == null) return true;
+        // 一个为空，一个非空，显然不同
+        if (root1 == null || root2 == null) return false;
+        // 两个都非空，但 val 不一样也不行
+        if (root1.val != root2.val) return false;
+
+        // root1 和 root2 该比的都比完了
+        return isSameTree(root1.left, root2.left)
+                && isSameTree(root1.right, root2.right);
+    }
+
+    /**
+     * 判断二叉树的合法性
+     */
+    private static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private static boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        return isValidBST(root.left, min, root)
+                && isValidBST(root.right, root, max);
+    }
+
+    /**
+     * 在二叉树中插入一个节点
+     */
+    private static TreeNode insertIntoBST(TreeNode root, int val) {
+        // 找到空位置插入新节点
+        if (root == null) return new TreeNode(val);
+        // if (root.val == val)
+        //     BST 中一般不会插入已存在元素
+        if (root.val < val)
+            root.right = insertIntoBST(root.right, val);
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+
+    /**
+     * 二叉树中删除一个节点
+     */
+    private static TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            // 这两个 if 把情况 1 和 2 都正确处理了
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            // 处理情况 3
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    private static TreeNode getMin(TreeNode node) {
+        // BST 最左边的就是最小的
+        while (node.left != null) node = node.left;
+        return node;
+    }
+
+}
+
+class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
