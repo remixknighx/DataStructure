2f47207209d90eaa285d2bfd674bacea577d5bbd
王剑峰 <remixknighx@sina.cn>
2020-05-17 22:42:49 +0800

    'leetcode'

--- src/main/java/com/leetcode/TreeNode.java
+++ src/main/java/com/leetcode/TreeNode.java
@@ -14,4 +14,10 @@ public class TreeNode {
         this.val = val;
     }
 
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
 }
