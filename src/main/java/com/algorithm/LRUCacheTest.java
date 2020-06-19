39edeae0acf4923f8b68750e0d3052e9f60ec49b
Wang Jianfeng <remixknighx@sina.cn>
2020-06-19 17:31:12 +0800

    'LRUCache调整'

--- src/main/java/com/algorithm/LRUCacheTest.java
+++ src/main/java/com/algorithm/LRUCacheTest.java
@@ -1,32 +1,37 @@
 package com.algorithm;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LRUCacheTest {
 
     public static void main(String[] args) {
-
+        LRUCache<String, String> lruCache = new LRUCache<>();
+        lruCache.put("1", "one");
+        lruCache.put("2", "two");
+        lruCache.put("3", "three");
+        lruCache.put("4", "four");
+        System.out.println(lruCache.get("1"));
     }
 
 }
 
-class DoubleList {
+class DoubleList<K> {
 
-    List<Node> innerList;
+    List<K> innerList;
 
     public DoubleList() {
     }
 
     public DoubleList(int capacity) {
-        this.innerList = new ArrayList<>(capacity);
+        this.innerList = new LinkedList<>();
     }
 
     /**
      * 在链表头部添加节点 x，时间 O(1)
      */
-    public void addFirst(Node x) {
+    public void addFirst(K x) {
         innerList.add(0, x);
     }
 
@@ -34,14 +39,14 @@ class DoubleList {
      * 删除链表中的 x 节点（x 一定存在）
      * 由于是双链表且给的是目标 Node 节点，时间 O(1)
      */
-    public void remove(Node x) {
+    public void remove(K x) {
         innerList.remove(x);
     }
 
     /**
      * 删除链表中最后一个节点，并返回该节点，时间 O(1)
      */
-    public Node removeLast() {
+    public K removeLast() {
         return innerList.remove(innerList.size() - 1);
     }
 
@@ -51,68 +56,85 @@ class DoubleList {
     public int size() {
         return innerList.size();
     }
+
+    public void clear() {
+        innerList.clear();
+    }
 }
 
-class LRUCache {
+class LRUCache<K, V> {
 
     /**
      * key -> Node(key, val)
      */
-    private HashMap<Integer, Node> map;
+    private HashMap<K, V> map;
     /**
      * Node(k1, v1) <-> Node(k2, v2)...
      */
-    private DoubleList cache;
+    private DoubleList<K> cache;
     /**
      * 最大容量
      */
     private int cap;
+    /**
+     * 默认容量
+     */
+    private static final int DEFAULT_CAPACITY = 3;
+
+    public LRUCache() {
+        this(DEFAULT_CAPACITY);
+    }
 
     public LRUCache(int capacity) {
         this.cap = capacity;
         map = new HashMap<>(this.cap);
-        cache = new DoubleList(this.cap);
+        cache = new DoubleList<>(this.cap);
     }
 
-    public synchronized int get(int key) {
+    public synchronized V get(K key) {
         if (!map.containsKey(key)) {
-            return -1;
+            return null;
         }
-        int val = map.get(key).val;
+        V val = map.get(key);
+        if (val == null) {
+            return null;
+        }
         // 利用 put 方法把该数据提前
-        put(key, val);
+        refreshCache(key, val);
         return val;
     }
 
-    public synchronized void put(int key, int val) {
+    public synchronized void put(K key, V val) {
         // 先把新节点 x 做出来
-        Node x = new Node(key, val);
-
         if (map.containsKey(key)) {
-            // 删除旧的节点，新的插到头部
-            cache.remove(map.get(key));
-            cache.addFirst(x);
-            // 更新 map 中对应的数据
-            map.put(key, x);
+            refreshCache(key, val);
         } else {
             if (cap == cache.size()) {
                 // 删除链表最后一个数据
-                Node last = cache.removeLast();
-                map.remove(last.key);
+                K lastKey = cache.removeLast();
+                map.remove(lastKey);
             }
             // 直接添加到头部
-            cache.addFirst(x);
-            map.put(key, x);
+            cache.addFirst(key);
+            map.put(key, val);
         }
     }
-}
 
-class Node {
-    public int key, val;
-    public Node next, prev;
-
-    public Node(int k, int v) {
-        this.key = k;
-        this.val = v;
+    public int size() {
+        return map.size();
     }
+
+    public synchronized void clear() {
+        cache.clear();
+        map.clear();
+    }
+
+    private void refreshCache(K key, V val) {
+        // 删除旧的节点，新的插到头部
+        cache.remove(key);
+        cache.addFirst(key);
+        // 更新 map 中对应的数据
+        map.put(key, val);
+    }
+
 }
