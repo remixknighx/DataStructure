774ca438543ba79b4ad9ec8ec88435139adaea69
王剑峰 <remixknighx@sina.cn>
2020-08-12 23:18:11 +0800

    leetcode

--- src/main/java/com/leetcode/MinCostClimbingStairs.java
+++ src/main/java/com/leetcode/MinCostClimbingStairs.java
@@ -6,12 +6,23 @@ package com.leetcode;
  */
 public class MinCostClimbingStairs {
 
+
 	public static void main(String[] args) {
-		
+		int[] cost = {10, 15, 20};
+
 	}
 
 	public int minCostClimbingStairs(int[] cost) {
-		return 0;
+		int n = cost.length;
+		int[] dp = new int[n];
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+		for (int i = 2; i < n; i++) {
+			dp[i] = Math.min((dp[i - 1] + cost[i]), (dp[i - 2] + cost[i]));
+		}
+		return Math.min(dp[n - 1], dp[n - 2]);
+
 	}
+
 
 }
