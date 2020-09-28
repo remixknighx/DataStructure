e5bcd5a2d40b48a05b4ab958e4dff3479e7d81ef
王剑峰 <remixknighx@sina.cn>
2020-09-28 23:20:58 +0800

    'search2dmatrix'

--- src/main/java/com/leetcode/medium/Search2DMatrixTwo.java
+++ src/main/java/com/leetcode/medium/Search2DMatrixTwo.java
@@ -13,11 +13,32 @@ public class Search2DMatrixTwo {
 				{10, 13, 14, 17, 24},
 				{18, 21, 23, 26, 30}};
 		System.out.println(new Search2DMatrixTwo().searchMatrix(matrix, 5));
-//		System.out.println(new Search2DMatrixTwo().searchMatrix(matrix, 20));
+		System.out.println(new Search2DMatrixTwo().searchMatrix(matrix, 20));
 	}
 
 
 	public boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix.length == 0 || matrix[0].length == 0) {
+			return false;
+		}
+
+		int row = matrix.length, col = matrix[0].length;
+		for (int i = 0; i < row; i++) {
+			if (target >= matrix[i][0] && target <= matrix[i][col-1]) {
+				int left = 0, right = col - 1;
+				while (left <= right) {
+					int mid = left + (right-left) / 2;
+					if (matrix[i][mid] > target) {
+						right = mid - 1;
+					} else if (matrix[i][mid] < target) {
+						left = mid + 1;
+					} else {
+						return true;
+					}
+				}
+			}
+		}
+
 		return false;
 	}
 
